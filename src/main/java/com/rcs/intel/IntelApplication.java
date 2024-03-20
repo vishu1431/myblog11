e8ce086d2c594f2a61fa6817c8047b62bfb906d4
vishal <vishalupadhyay1431@gmail>
2024-03-20 11:32:55 +0530

    125:first commit by me

---
+++ src/main/java/com/rcs/intel/IntelApplication.java
@@ -0,0 +1,13 @@
+package com.rcs.intel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rcs/intel/IntelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rcs.intel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
